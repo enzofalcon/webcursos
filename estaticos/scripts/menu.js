b0f7e6984051bb8c3d8d09944e8481c94b2af22e
Enzo Falcón <enalfaga@gmail.com>
2025-02-28 01:19:04 -0300

    menu2

--- estaticos/scripts/menu.js
+++ estaticos/scripts/menu.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuContainer = document.getElementById("menu-container");
 
     if (menuContainer) {
-        fetch("menu.html")
+        fetch("/webcursos/menu.html")
+
             .then(response => {
                 if (!response.ok) throw new Error("No se pudo cargar el menú");
                 return response.text();
